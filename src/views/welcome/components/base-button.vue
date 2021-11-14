4685c2349e9dda5711a6fbf5cc139075c1998146
ShookLyngs <shook-lyngs@qq.com>
2021-11-14 13:38:01 +0800

    update(welcome/component): updated base-button props.

--- src/views/welcome/components/base-button.vue
+++ src/views/welcome/components/base-button.vue
@@ -1,29 +1,48 @@
+<template>
+  <component class="button-base" :is="tag">
+    <slot />
+  </component>
+</template>
+
 <script>
-  import { h } from 'vue';
+  // Functions
+  import { computed } from 'vue';
 
   export default {
     name: 'base-button',
     props: {
-      tag: {
-        type: String,
-        default: 'button',
+      link: {
+        type: Boolean,
+        default: false,
       },
     },
-    setup(props, { slots }) {
-      return () => h(
-        props.tag,
-        { class: 'button' },
-        slots.default?.()
-      );
+    setup(props, { attrs }) {
+      const tag = computed(() => {
+        if (props.link) {
+          return 'to' in attrs ? 'router-link' : 'a';
+        } else {
+          return 'button';
+        }
+      });
+
+      return {
+        tag,
+      };
     },
   };
 </script>
 
 <style lang="less" scoped>
-  .button {
-    @apply inline-block text-center align-middle body-x h-14 text-xl font-bold transition cursor-pointer select-none;
-  }
-  a.button {
-    @apply inline-flex justify-center items-center;
+  .button-base {
+    @apply inline-block text-center align-middle cursor-pointer select-none;
+    @apply body-x h-14 text-xl font-bold transition;
+
+    a& {
+      @apply inline-flex justify-center items-center;
+    }
+
+    &.filled {
+      @apply text-negative-900 bg-theme-500 hover:opacity-80 active:opacity-60;
+    }
   }
 </style>
